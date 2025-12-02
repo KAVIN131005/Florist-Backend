3f3d77c77755d313a3e59c1727d3efd5b69e61f7
KAVIN1315 <kumarkavin2005@gmail.com>
2025-12-02 23:49:52 +0530

    commit

--- src/main/java/com/example/backend/controller/HealthController.java
+++ src/main/java/com/example/backend/controller/HealthController.java
@@ -1,12 +1,12 @@
 package com.example.backend.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
